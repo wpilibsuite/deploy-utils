ebc88a7d8f1ad9f0c2936dd61f2d1b1165370ac6
Thad House <thadhouse1@gmail.com>
2021-02-19 18:15:24 -0800

    Disable tests remove files and toolchains

--- src/main/java/edu/wpi/first/deployutils/files/AbstractDirectoryTree.java
+++
@@ -1,9 +0,0 @@
-package edu.wpi.first.deployutils.files;
-
-public abstract class AbstractDirectoryTree implements IDirectoryTree {
-
-    public IDirectoryTree plus(IDirectoryTree other) {
-        return new CombinedDirectoryTree(this, other);
-    }
-
-}
